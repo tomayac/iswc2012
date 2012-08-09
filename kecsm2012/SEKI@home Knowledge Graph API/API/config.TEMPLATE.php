a2516a063f91040c5eb16b00d20bb61c9dcfa0de
smirea <steven.mirea@gmail.com>
2012-08-09 21:15:02 +0200

    changed MYSQL -> MYSQLI and added a global constant DEBUG variable

--- kecsm2012/SEKI@home Knowledge Graph API/API/config.TEMPLATE.php
+++ kecsm2012/SEKI@home Knowledge Graph API/API/config.TEMPLATE.php
@@ -1,5 +1,7 @@
 <?php
 
+  define('DEBUG', TRUE);
+
   define('DB_USER', 'a_username');
   define('DB_PASS', 'a_password');
   define('DB_NAME', 'a_name');
@@ -10,20 +12,28 @@
   define('TABLE_OBJECTS', 'objects');
 
 
-  dbConnect(DB_USER, DB_PASS, DB_NAME, DB_HOST);
+  $DB = mysqliConnect(DB_USER, DB_PASS, DB_NAME, DB_HOST);
 
 
   /**
-   * @brief Perform a database connection
+   * @brief Returns a MSQLI Class database connection
    * @warning Dies if it is unable to make a connection
    * @param {string} $user
    * @param {string} $pass
    * @param {string} $name
    * @param {string} $host
+   * @return {MYSQLI}
    */
-  function dbConnect($user, $pass, $name = null, $host = 'localhost'){
-    $connexion = mysql_connect( $host, $user, $pass ) or die ("Could not connect to Data Base!");
-    if( $name ) mysql_select_db( $name, $connexion ) or die ("Failed to select Data Base");
+  function msqliConnect($user, $pass, $name, $host = 'localhost'){
+    $mysqli = new mysqli($host, $user, $pass, $name);
+
+    /* check connection */
+    if ($mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+    }
+
+    return $mysqli;
   }
 
 ?>
