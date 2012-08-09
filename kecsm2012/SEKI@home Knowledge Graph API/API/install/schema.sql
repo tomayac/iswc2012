9b1b8642dadeb180427046c3e703856e52915391
smirea <steven.mirea@gmail.com>
2012-08-09 20:02:27 +0200

    updated schema

--- kecsm2012/SEKI@home Knowledge Graph API/API/install/schema.sql
+++ kecsm2012/SEKI@home Knowledge Graph API/API/install/schema.sql
@@ -2,14 +2,11 @@
 DROP TABLE IF EXISTS `subjects`;
 CREATE TABLE `subjects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sid` varchar(64) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `wiki` varchar(128) NOT NULL,
+  `oid` varchar(128) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `wiki` varchar(256) NOT NULL,
   PRIMARY KEY (id),
-  INDEX (sid),
-  INDEX (name),
-  INDEX (wiki),
-  CONSTRAINT _sid UNIQUE (sid),
+  CONSTRAINT _oid UNIQUE (oid),
   CONSTRAINT _name UNIQUE (name),
   CONSTRAINT _wiki UNIQUE (wiki)
 ) AUTO_INCREMENT=1;
@@ -31,6 +28,5 @@ CREATE TABLE `objects` (
   PRIMARY KEY (id),
   INDEX (sid),
   INDEX (pid),
-  INDEX (value),
   CONSTRAINT _value UNIQUE (value)
 ) AUTO_INCREMENT=1;
