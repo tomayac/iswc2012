c9625945020321613d8c795208bce99dd410bc40
smirea <steven.mirea@gmail.com>
2012-08-10 21:43:51 +0200

    working

--- kecsm2012/SEKI@home Knowledge Graph API/API/storage/index.php
+++ kecsm2012/SEKI@home Knowledge Graph API/API/storage/index.php
@@ -4,25 +4,13 @@
   require_once '../config.php';
   require_once '../utils.php';
   require_once '../jsonld/jsonld.php';
-  require_once '../arc2/ARC2.php';
+  require_once '../setup_store.php';
 
   $graph_uri = 'http://openknowledgegraph.org';
 
   $jsonld_options = array(
     'format' => 'application/nquads'
   );
-
-  $store = ARC2::getStore(array(
-    'db_host' => DB_HOST,
-    'db_name' => DB_NAME,
-    'db_user' => DB_USER,
-    'db_pwd' => DB_PASS,
-    'store_name' => 'SEKI'
-  ));
-
-  if (!$store->isSetUp()) {
-    $store->setUp();
-  }
 
   $data = $_REQUEST;
   //cleanData($data);
