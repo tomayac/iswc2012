c06d9ff53c7c64badb6f763c43d683362732496f
smirea <steven.mirea@gmail.com>
2012-08-10 18:19:31 +0200

    simpler config file

--- kecsm2012/SEKI@home Knowledge Graph API/API/config.TEMPLATE.php
+++ kecsm2012/SEKI@home Knowledge Graph API/API/config.TEMPLATE.php
@@ -7,33 +7,4 @@
   define('DB_NAME', 'a_name');
   define('DB_HOST', 'localhost');
 
-  define('TABLE_SUBJECTS', 'subjects');
-  define('TABLE_PREDICATES', 'predicates');
-  define('TABLE_OBJECTS', 'objects');
-
-
-  $DB = mysqliConnect(DB_USER, DB_PASS, DB_NAME, DB_HOST);
-
-
-  /**
-   * @brief Returns a MSQLI Class database connection
-   * @warning Dies if it is unable to make a connection
-   * @param {string} $user
-   * @param {string} $pass
-   * @param {string} $name
-   * @param {string} $host
-   * @return {MYSQLI}
-   */
-  function mysqliConnect($user, $pass, $name, $host = 'localhost'){
-    $mysqli = new mysqli($host, $user, $pass, $name);
-
-    /* check connection */
-    if ($mysqli->connect_errno) {
-      printf("Connect failed: %s\n", $mysqli->connect_error);
-      exit();
-    }
-
-    return $mysqli;
-  }
-
 ?>
