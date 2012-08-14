b2eb0c0590a145af7cf08ad8346aaa569960df20
smirea <steven.mirea@gmail.com>
2012-08-14 18:51:22 +0200

    added get_web_page function

--- kecsm2012/SEKI@home Knowledge Graph API/API/utils.php
+++ kecsm2012/SEKI@home Knowledge Graph API/API/utils.php
@@ -1,6 +1,39 @@
 <?php
 
   /**
+   * Get a web file (HTML, XHTML, XML, image, etc.) from a URL.  Return an
+   * array containing the HTTP server response header fields and content.
+   * @param {String} $url
+   * @return {Array}
+   */
+  function get_web_page ($url) {
+    $options = array(
+      CURLOPT_RETURNTRANSFER => true,     // return web page
+      CURLOPT_HEADER         => false,    // don't return headers
+      CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+      CURLOPT_ENCODING       => "",       // handle all encodings
+      CURLOPT_USERAGENT      => "spider", // who am i
+      CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+      CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+      CURLOPT_TIMEOUT        => 120,      // timeout on response
+      CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+    );
+
+    $ch = curl_init( $url );
+    curl_setopt_array( $ch, $options );
+    $content = curl_exec( $ch );
+    $err = curl_errno( $ch );
+    $errmsg = curl_error( $ch );
+    $header = curl_getinfo( $ch );
+    curl_close( $ch );
+
+    $header['errno']   = $err;
+    $header['errmsg']  = $errmsg;
+    $header['content'] = $content;
+    return $header;
+  }
+
+  /**
    * @param {String|Array} @data
    */
   function cleanData (&$data) {
@@ -54,7 +87,7 @@
     }
     jsonOutput($object);
   }
-  
+
   function turtleOutput($store, $rows) {
     if (!headers_sent()) {
       header('Cache-Control: no-cache, must-revalidate');
