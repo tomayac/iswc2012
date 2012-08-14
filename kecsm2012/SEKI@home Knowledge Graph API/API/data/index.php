0ed925a1500b1b0b44c28f85b95eb699a00fb35e
smirea <steven.mirea@gmail.com>
2012-08-14 19:53:45 +0200

    now just loads sparql.php with a custom query

--- kecsm2012/SEKI@home Knowledge Graph API/API/data/index.php
+++ kecsm2012/SEKI@home Knowledge Graph API/API/data/index.php
@@ -4,21 +4,23 @@
 
   require_once '../config.php';
   require_once '../utils.php';
-  require_once '../jsonld/jsonld.php';
-  require_once '../setup_store.php';
 
   if (isset($_GET['query'])) {
     $dirname = dirname($_SERVER['REQUEST_URI']);
     $base = implode('/', array_slice(explode('/', $dirname), 0, -1));
-    $url = WEB_ROOT . $base . '/sparql.php?query=' .
-                      urldecode(
-                        'SELECT ?predicate ?object WHERE { ' .
-                          $_GET['query'] . ' ?predicate ?object ' .
-                        '}'
-                      );
-    v_export($url);
-    echo '<hr />';
+    $query = 'SELECT ?predicate ?object ' .
+              "WHERE { \n" .
+                '<http://openknowledgegraph.org/data/'.$_REQUEST['query'].">\n".
+                " ?predicate ?object . \n".
+              '}';
+    $url = WEB_ROOT . $base . '/sparql.php?query=' . urlencode($query);
     $page = get_web_page($url);
+    if (!headers_sent()) {
+      $headers = explode("\r\n", $page['headers']);
+      foreach ($headers as $header) {
+        header($header);
+      }
+    }
     echo $page['content'];
   } else {
     outputError('Invalid identifier');
