5be55135377fbad0d14442d1c3aeda329c8e038d
smirea <steven.mirea@gmail.com>
2012-08-16 15:55:47 +0200

    last added triple live table in /sparql

---
+++ kecsm2012/SEKI@home Knowledge Graph API/API/js/lastTriple.js
@@ -0,0 +1,45 @@
+
+var stats_url = 'http://seki.code4fun.de/stats';
+var interval = 5 * 1000;
+
+var lastTotal = -1;
+$(function _popupWindowOnLoad () {
+
+  var $total = $('#total');
+  var $lastTriples = $('#lastTriples');
+
+  updateInfo();
+  updateInterval = setInterval(updateInfo, interval);
+
+  function updateInfo () {
+    $.get(stats_url, function _onResponse (result) {
+      if (result.total === lastTotal) {
+        return;
+      }
+      lastTotal = result.total;
+      if (result.subject) {
+        var middle = result.subject.lastIndexOf('/') + 1;
+        result.subject = '<a href="'+result.subject+'" target="_blank" ' +
+                            'title="click to download" >' +
+                            result.subject.slice(0, middle) + ' ' +
+                            result.subject.slice(middle) +
+                          '</a>';
+      }
+      $total.html(result.total);
+      var wrapper = $(document.createElement('tbody'));
+      wrapper.attr('id', result.total).addClass('triple');
+      delete result.total;
+
+      for (var key in result) {
+        wrapper.append( '<tr>' +
+                          '<td>' + key + '</td>' +
+                          '<td class="'+ key +'">' + result[key] +
+                        '</td>'
+        );
+      }
+
+      $lastTriples.append(wrapper);
+    });
+  }
+
+});
--- kecsm2012/SEKI@home Knowledge Graph API/API/stats.php
+++ kecsm2012/SEKI@home Knowledge Graph API/API/stats.php
@@ -1,5 +1,7 @@
 <?php
 
+  header("Access-Control-Allow-Origin: *");
+
   require_once 'config.php';
   require_once 'utils.php';
 
