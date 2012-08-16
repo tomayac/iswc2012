a9cf715d5b774dc34dfa4a13f4028784d9afa614
smirea <steven.mirea@gmail.com>
2012-08-16 14:09:17 +0200

    added DB_STORE_NAME global variable

--- kecsm2012/SEKI@home Knowledge Graph API/API/config.TEMPLATE.php
+++ kecsm2012/SEKI@home Knowledge Graph API/API/config.TEMPLATE.php
@@ -6,6 +6,7 @@
   define('DB_PASS', 'a_password');
   define('DB_NAME', 'a_name');
   define('DB_HOST', 'localhost');
+  define('DB_STORE_NAME', 'SEKI');
 
   // no leading slash
   define('WEB_ROOT', 'http://localhost');
